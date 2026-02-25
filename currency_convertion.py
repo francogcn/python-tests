86c4d16fc8aa6f120c963007df528c504e392f24
francogcn <88166836+francogcn@users.noreply.github.com>
2026-02-25 17:02:37 -0300

    Refactor currency conversion to use DolarHoy class
    Updated currency conversion logic to use DolarHoy class for fetching the official dollar price.

--- currency_convertion.py
+++ currency_convertion.py
@@ -1,13 +1,16 @@
 #! python3
 
 #Importacion de modulos
-import dolarhoy
+from dolarhoy import DolarHoy
+
+#Instancio la clase DolarHoy
+dolar_hoy = DolarHoy()
+
+#El valor de la cotizacion se obtiene mediante un fetch del modulo dolarhoy
+cotizacion = dolar_hoy.get_oficial_price()
 
 #Se pide al usuario ingresar el monto a convertir
 moneda = int(input("Ingrese el monto en pesos: "))
 
-#El valor de la cotizacion se obtiene mediante un fetch del modulo dolarhoy
-cotizacion = dolarhoy.get_dolarhoy()
-
 #Se imprime el monto convertido
-print(f"AR ${moneda} son U$D {moneda*cotizacion}")
+print(f"AR ${moneda} son U$D {moneda*cotizacion['venta']}")

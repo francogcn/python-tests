003ab6f895da0d745b7614029f5c3c5f2872a99d
francogcn <88166836+francogcn@users.noreply.github.com>
2026-02-28 18:39:06 -0300

    Enhance code readability with comments
    Added comments for clarity and improved code readability.

--- dolar_mep_hoy.py
+++ dolar_mep_hoy.py
@@ -1,8 +1,18 @@
+#Import required libraries
 import requests, bs4
+
+#Send HTML request to dolarhoy.com
 res = requests.get("https://dolarhoy.com/cotizacion-dolar-bolsa")
+
+#Program halts if download is fails
 res.raise_for_status()
+
 # Parses the content if the request succeeds
 soup = bs4.BeautifulSoup(res.text, "html.parser")
+
+#Selects html elment with class .cotizacion_value
 mep_dolar = soup.select(".cotizacion_value")
+
+#Prints the current value for mep dolar
 for x in range(len(mep_dolar)):
   print(mep_dolar[x].get_text(separator='\n'))

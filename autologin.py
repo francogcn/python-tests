387cf439555f82d3bd5b8819b7443cf9c685348b
francogcn <88166836+francogcn@users.noreply.github.com>
2026-03-01 18:39:33 -0300

    Make login URL configurable via user input

--- autologin.py
+++ autologin.py
@@ -1,6 +1,7 @@
 #AUTO LOGIN
 import requests
 
+url = str(input('LOGIN_URL'))
 # Fill in your details here to be posted to the login form.
 payload = {
     'inUserName': 'username',
@@ -9,7 +10,7 @@ payload = {
 
 # Use 'with' to ensure the session context is closed after use.
 with requests.Session() as s:
-    p = s.post('LOGIN_URL', data=payload)
+    p = s.post(url, data=payload)
     # Print the HTML returned or something more intelligent
     # to see if it's a successful login page.
     print(p.text)

a9437a32fd4ffc2e4edbc22805b5a67163cb860e
francogcn <88166836+francogcn@users.noreply.github.com>
2026-03-11 11:17:46 -0300

    Update weather_scrap.py

--- weather_scrap.py
+++ weather_scrap.py
@@ -1,8 +1,12 @@
-#Import main modules
 import requests, bs4
-#Download content from weather.com for city 'Posadas, Misiones'
-res = requests.get('https://weather.com/es-AR/tiempo/hoy/l/2d06ed300257681d1441a7014c3f72deb9e6073a1a998679ee383c23849ba0e5')
+#Download content from weather.com for city 'Posadas, Misiones' or 'Wanda, Misiones'
 
+ubi = input("Ingrese la ubicacion: ")
+ubi = ubi.lower().strip()
+if ubi == "posadas":
+  res = requests.get('https://weather.com/es-AR/tiempo/hoy/l/2d06ed300257681d1441a7014c3f72deb9e6073a1a998679ee383c23849ba0e5')
+else:
+  res = requests.get('https://weather.com/es-AR/tiempo/hoy/l/18e6e7c0ded65486463277de9179836591565039682af2f710c2658222f41895')
 #Program halts if download fails
 res.raise_for_status()
 
@@ -17,8 +21,8 @@ feels_like = soup.select('.TodayDetailsCard--feelsLikeTempValue--8WgHV')
 
 #If elements are found, prints message with current temperature and what it feels like
 if len(temperature) > 0 and len(feels_like) > 0:
-  print(f"Temperature in Posadas is {temperature[0].getText()}C")
-  print(f"Feels like: {fells_like[0].getText()}C")
-  print("Source: Weather.com")
+  print(f"Temperature in {ubi.capitalize()} is {temperature[0].getText()}C")
+  print(f"Feels like: {feels_like[0].getText()}C")
+  print("Source: weather.com")
 else:
   print("Search failed")
